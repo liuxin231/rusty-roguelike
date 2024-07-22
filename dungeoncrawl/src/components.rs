d0cfc582b604e333bac47af5f6d3b33fd8d924f6
liuxin <dove__lx@aliyun.com>
2024-07-22 13:34:53 +0800

    first commit

---
+++ dungeoncrawl/src/components.rs
@@ -0,0 +1,90 @@
+pub use crate::prelude::*;
+use std::collections::HashSet;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ActivateItem {
+    pub used_by: Entity,
+    pub item: Entity,
+}
+
+#[derive(Clone, PartialEq)]
+pub struct Carried(pub Entity);
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct ProvidesHealing {
+    pub amount: i32,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct ProvidesDungeonMap;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Render {
+    pub color: ColorPair,
+    pub glyph: FontCharType,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Player {
+    pub map_level: u32,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Enemy;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct MovingRandomly;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct WantsToMove {
+    pub entity: Entity,
+    pub destination: Point,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Health {
+    pub current: i32,
+    pub max: i32,
+}
+
+#[derive(Clone, PartialEq)]
+pub struct Name(pub String);
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct WantsToAttack {
+    pub attacker: Entity,
+    pub victim: Entity,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct ChasingPlayer;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Item;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct AmuletOfYala;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct FieldOfView {
+    pub visible_tiles: HashSet<Point>,
+    pub radius: i32,
+    pub is_dirty: bool,
+}
+
+impl FieldOfView {
+    pub fn new(radius: i32) -> Self {
+        Self {
+            visible_tiles: HashSet::new(),
+            radius,
+            is_dirty: true,
+        }
+    }
+
+    pub fn clone_dirty(&self) -> Self {
+        Self {
+            visible_tiles: HashSet::new(),
+            radius: self.radius,
+            is_dirty: true,
+        }
+    }
+}
